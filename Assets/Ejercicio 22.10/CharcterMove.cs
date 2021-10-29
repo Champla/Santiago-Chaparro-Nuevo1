7e3b974c3c6bbde8497ee437ea1a64bf1f51b1e7
Champla <89432746+Champla@users.noreply.github.com>
2021-10-29 15:18:34 -0500

    Caminado animacion
    trabajo 29/10/2021

--- Assets/Ejercicio 22.10/CharcterMove.cs
+++ Assets/Ejercicio 22.10/CharcterMove.cs
@@ -6,24 +6,51 @@ public class CharcterMove : MonoBehaviour
 {
     public float Speed = 5.0f;
     public float Speedr = 200.0f;
+    public Rigidbody rb;
+    public float JumpForce = 8f;
+    public bool puedoSaltar;
 
     private Animator anim;
     public  float x,y;
     void Start()
     {
+        puedoSaltar = false;
         anim = GetComponent<Animator>();
     }
 
-   
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * Speedr, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * Speed);
+    }
+
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-
-        transform.Rotate(0, x * Time.deltaTime * Speedr, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * Speed);
+              
 
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
+
+        if (puedoSaltar) 
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                anim.SetBool("Salte",true);
+                rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("tocoSuelo", true);
+        }
+        else 
+        {
+            EstoyCayendo();
+        }
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocoSuelo", false);
+        anim.SetBool("salte", false);
     }
 }
